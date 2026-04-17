3282dbd5a98cffb8548ecc68be0c47ba4d2bd5c3
Flavio <flavio2405@gmail.com>
2026-04-17 18:10:14 +0200

    generator.py per la generazione della sinusoide

--- Generator/generator.py
+++ Generator/generator.py
@@ -1,17 +1,15 @@
 import numpy as np
 import sounddevice as sd
-import time
 
 sps = 44100
-freq_hz = 10000
-duration_s = 5.0
-attenuation = 0.3
+freq_hz = 1000
+duration_s = 3.0
+attenuation = 0.5
 
 each_sample_number = np.arange(duration_s * sps)
 
 waveform = np.sin(2 * np.pi * each_sample_number * freq_hz / sps)
 waveform_quiet = waveform * attenuation
 
-sd.play(waveform_quiet, sps)
-time.sleep(duration_s)
-sd.stop()
+sd.play(waveform_quiet, sps, device = 'ESP32-OTG')
+sd.wait()
